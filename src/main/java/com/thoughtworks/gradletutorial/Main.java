a6a04a76c9e87abe66575ed6110cdd8334ee854c
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-01-14 22:32:36 +0800

    build jar with main class

---
+++ src/main/java/com/thoughtworks/gradletutorial/Main.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.gradletutorial;
+
+import java.util.Arrays;
+
+/**
+ * Created by longyang.zhang on 1/14/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
