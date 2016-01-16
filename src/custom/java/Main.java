1766748a7aaa9c0aa6eb595b61dee1d0c63de433
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-01-16 12:09:02 +0800

    add src/custom/java to source set

---
+++ src/custom/java/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Created by longyang.zhang on 1/14/16.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(Arrays.toString(args));
+    }
+}
