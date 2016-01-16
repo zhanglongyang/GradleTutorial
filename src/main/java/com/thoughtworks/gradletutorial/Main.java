acd85fafe4022f0458323f13886848917ddf5ed2
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-01-16 11:51:31 +0800

    add figlet dependency

--- src/main/java/com/thoughtworks/gradletutorial/Main.java
+++ src/main/java/com/thoughtworks/gradletutorial/Main.java
@@ -1,12 +1,16 @@
 package com.thoughtworks.gradletutorial;
 
-import java.util.Arrays;
+import java.io.IOException;
+import com.github.lalyos.jfiglet.FigletFont;
 
 /**
  * Created by longyang.zhang on 1/14/16.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
+    public static void main(String[] args) throws IOException {
+        for (String arg : args) {
+            String asciiString = FigletFont.convertOneLine(arg);
+            System.out.println(asciiString);
+        }
     }
 }
